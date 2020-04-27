5c8950fbc353d68d8f18c51b9f9f292f6a4661dc
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-27 19:52:34 +0900

    Update node.js

--- js/node.js
+++ js/node.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
 
+const attendanceButtonId = document.getElementById("attendance");
+const leavingButtonId = document.getElementById("leaving");
+
+attendanceButtonId.addEventListener("click", function() {
+    var now = new Date();
+    var Year = now.getFullYear();
+    var Month = now.getMonth()+1;
+    var Date = now.getDate();
+    var Hour = now.getHours();
+    var Min = now.getMinutes();
+    fs.appendFileSync('/daikuru.github.io/attendance/attendance.txt', Year + "/" + Month + "/" + Date + "日" + Hour + ":" + Min + ":" );
+    console.log('attendance.txtに追記されました');
+});
