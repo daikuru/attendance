ebe595df02a5c1f24c52fed059bb0b51b8fc5b36
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-27 16:25:23 +0900

    Update cashFileSetting.js

--- cashFileSetting.js
+++ cashFileSetting.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+
+const attendanceButtonId = document.getElementById("attendance");
+const leavingButtonId = document.getElementById("leaving");
+
 // キャッシュファイルの指定
 // 出勤キャッシュ
 var CACHE_ATTENDANCE = 'attendance-cache';
@@ -5,6 +10,8 @@ var CACHE_ATTENDANCE = 'attendance-cache';
 var CACHE_LEAVING = 'leaving-cache';
 var urlsToCache = [
     '/daikuru.github.io/attendance/',
+    '/daikuru.github.io/attendance/attendance.txt/,
+    '/daikuru.github.io/attendance/leaving.txt/,
 ];
 
 // インストール処理
@@ -23,6 +30,20 @@ self.addEventListener('install', function(event) {
               // 1つでも失敗したらService Workerのインストールはスキップされる
           })
     );
+});
+
+attendanceButtonId.addEventListener("click", function() {
+    var now = new Date();
+    var Year = now.getFullYear();
+    var Month = now.getMonth()+1;
+    var Date = now.getDate();
+    var Hour = now.getHours();
+    var Min = now.getMinutes();
+    fs.appendFileSync('attendance.txt', Year + "/" + Month + "/" + Date + "日" + Hour + ":" + Min + ":" );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(new Response('Hello World!'));
 });
 
 // リソースフェッチ時のキャッシュロード処理
