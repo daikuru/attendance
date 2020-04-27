eb7efffc64775a80b263fa51e1e1cbdd4b28b194
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-27 13:40:54 +0900

    Update cashFileSetting.js

--- cashFileSetting.js
+++ cashFileSetting.js
@@ -1,17 +1,27 @@
 // キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
+// 出勤キャッシュ
+var CACHE_ATTENDANCE = 'attendance-cache';
+// 退勤キャッシュ
+var CACHE_LEAVING = 'leaving-cache';
 var urlsToCache = [
-    '/poster-keisuke.github.io/',
+    '/daikuru.github.io/attendance/',
 ];
 
 // インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_ATTENDANCE)
+          .then(
+          function(cache){
+              return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
+              // 1つでも失敗したらService Workerのインストールはスキップされる
+          })
+        caches.open(CACHE_LEAVING)
+          .then(
+          function(cache){
+              return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
+              // 1つでも失敗したらService Workerのインストールはスキップされる
+          })
     );
 });
 
