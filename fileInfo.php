3e17c717616423692c99a53eac611dbe5d952e05
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-27 23:31:17 +0900

    Create fileInfo.php

---
+++ fileInfo.php
@@ -0,0 +1,11 @@
+<?php
+if($_POST['attendanceInfo']=="出勤"){
+$fp = fopen("/attendance/attendance.txt", "a");
+fwrite($fp, .date("Y/m/d H:i:s")."¥n");
+}elseif($_POST['attendanceInfo']=="退勤"){
+$fp = fopen("/attendance/leaving.txt", "a");
+fwrite($fp, .date("Y/m/d H:i:s")."¥n");
+}else{
+
+}
+?>
