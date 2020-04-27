0c0b6799a1dda0e596728e28c7b8e0eed89b1d2d
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-27 18:14:23 +0900

    Update cashFileSetting.js

--- cashFileSetting.js
+++ cashFileSetting.js
@@ -1,67 +1,11 @@
-// const fs = require('fs');
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
-// const attendanceButtonId = document.getElementById("attendance");
-// const leavingButtonId = document.getElementById("leaving");
-
-// キャッシュファイルの指定
-// 出勤キャッシュ
-// var CACHE_ATTENDANCE = 'attendance-cache';
-// 退勤キャッシュ
-// var CACHE_LEAVING = 'leaving-cache';
-
-var CACHE_NAME = 'attendance-cache';
-
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-    '/daikuru.github.io/attendance/',
-    '/daikuru.github.io/attendance/attendance.txt/',
-    '/daikuru.github.io/attendance/leaving.txt/',
+	'/.github.io/',
 ];
-/*
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then(
-          function(cache){
-              // return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
-              return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-              // 1つでも失敗したらService Workerのインストールはスキップされる
-          })
-    );
-});
-*/
-
-/*
-attendanceButtonId.addEventListener("click", function() {
-    var now = new Date();
-    var Year = now.getFullYear();
-    var Month = now.getMonth()+1;
-    var Date = now.getDate();
-    var Hour = now.getHours();
-    var Min = now.getMinutes();
-    fs.appendFileSync('/daikuru.github.io/attendance/attendance.txt', Year + "/" + Month + "/" + Date + "日" + Hour + ":" + Min + ":" );
-    console.log('attendance.txtに追記されました');
-});
-*/
-
-/*
-self.addEventListener('fetch', event => {
-  event.respondWith(new Response('Hello World!'));
-});
-*/
-/*
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
-*/
 // インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
