58c75c3fe83a7780e0fbd6f196dccd94a156e7d8
daikuru <63463963+daikuru@users.noreply.github.com>
2020-04-29 23:03:40 +0900

    Update fileInfo.php

--- fileInfo.php
+++ fileInfo.php
@@ -1,4 +1,6 @@
 <?php
+//ボタンを押下した時の動作を種類ごと行う
+//各テキストファイルに追記モードで日付を記載していく
 if($_POST['attendanceInfo']=="出勤"){
 $fp = fopen("/attendance/attendance.txt", "a");
 fwrite($fp, .date("Y/m/d H:i:s")."¥n");
@@ -8,6 +10,6 @@ $fp = fopen("/attendance/leaving.txt", "a");
 fwrite($fp, .date("Y/m/d H:i:s")."¥n");
 fclose($fp);
 }else{
-
+//特に何もしない
 }
 ?>
